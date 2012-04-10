3c7894ec7be8ef8ce9042ee7fb1f70188d044132
Corey Schooler <cschooler@cerner.com>
2012-04-10 12:08:00 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Harmonia::Application.config.secret_token = '1d656dc88e9cef155fcd54495e0af9a8ac01427beedaeb85329ba111cb2fe106ba51afd907bffe182cac54d2e3d7fdf321c853c09643634499308e2d3ffa4fe6'
