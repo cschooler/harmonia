fec2f34913ba90ed7f3830acba4c342e4e814f44
Corey Schooler <cschooler@cerner.com>
2012-04-18 21:15:36 -0500

    added manage event flows

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,17 +1,27 @@
 class EventsController < ApplicationController
   def index
     @title = 'Events'
+    @events = Event.all
   end
   
   def show
+    @event = Event.find(params[:id])
     @title = 'Event'
   end
   
   def new
     @title = 'Create New Event'
+    @event = Event.new
   end
   
   def create
-    
+    @event = Event.new(params[:event])
+    if @event.save
+      flash[:success] = "Event Created!"
+      redirect_to @event
+    else
+      @title = "Create New Event"
+      render 'new'
+    end
   end
 end
